109781119eddeced6e9d1c06fc887665e5f851da
CorentinDumery <corentin.dumery@gmail.com>
2019-05-09 17:00:44 +0200

    Trying to work here

--- experience.py
+++ experience.py
@@ -14,9 +14,9 @@ from starting_block import manage_time_elapsed
 
 switch_pin_bcm    = gpio.gpio_index_of_wpi_pin    (22)
 
-gpio.set_pull_up_down(  color_button_pin_bcm,     gpio.PULL_UP)
-gpio.set_pin_mode(      color_button_pin_bcm,     gpio.INPUT)
+gpio.set_pull_up_down(  switch_pin_bcm,     gpio.PULL_UP)
+gpio.set_pin_mode(      switch_pin_bcm,     gpio.INPUT)
 
 while 1:
-    if gpio.digital_read(color_button_pin_bcm) == 1:
+    if gpio.digital_read(switch_pin_bcm) == 1:
         print("yes !")
